a691a6c07ae1a49dfe4d9da1312bba233387fea1
xiongkun116276 <1162767656@qq.com>
2021-07-06 09:39:47 +0800

    target字段后面是空的运行报错

--- vue.config.js
+++ vue.config.js
@@ -37,7 +37,7 @@ module.exports = {
     open: true, //配置自动启动浏览器
     proxy: {// 配置多个代理(配置一个 proxy: 'http://localhost:4000' )
       '/api': {
-        target: '',
+        target: 'http://localhost:8081',
         changeOrigin: true,
         pathRewrite: {
           "^/api": ""
